72d1985f88e037f7f5631d8387266090f26bf8f4
ImaFruitSnack <4yrmaddict@gmail.com>
2023-12-28 13:28:05 -0600

    Update

---
+++ Automate_for_me.bat
@@ -0,0 +1,8 @@
+git init
+git pull --rebase origin main 
+git remote remove origin
+git add .
+git commit -m "Update"
+git branch -M main
+git remote add origin git@github.com:ImaFruitSnack/ArtCart.git
+git push -u origin main
